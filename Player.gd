69e32e0158fc551b296fd0b6b5d79e487289cc96
Martin Castel <martin.castel@hotmail.fr>
2021-01-30 00:49:22 +0100

    add friction and acceleration

--- Player.gd
+++ Player.gd
@@ -7,12 +7,26 @@ export (int) var gravity = 4000
 var velocity = Vector2.ZERO
 var double_jumped = false
 
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
+
 func get_input():
-	velocity.x = 0
+	var dir = 0
 	if Input.is_action_pressed("walk_right"):
-		velocity.x += speed
+		dir += 1
 	if Input.is_action_pressed("walk_left"):
-		velocity.x -= speed
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+
+#func get_input():
+#	velocity.x = 0
+#	if Input.is_action_pressed("walk_right"):
+#		velocity.x += speed
+#	if Input.is_action_pressed("walk_left"):
+#		velocity.x -= speed
 
 func _physics_process(delta):
 	get_input()
