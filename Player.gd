dec5b8c50befdf47092495ce6aab669072b62408
Martin Castel <martin.castel@hotmail.fr>
2021-01-30 02:53:21 +0100

    add hover, kind of

--- Player.gd
+++ Player.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 
 export (int) var speed = 400
+export (int) var hover_speed = 400
 export (int) var jump_speed = -1100
 export (int) var gravity = 4000
 
 var velocity = Vector2.ZERO
-var double_jumped = false
+var has_double_jumped = false
 
 export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var air_friction = 0.5
 export (float, 0, 1.0) var acceleration = 0.25
 
 func get_input():
@@ -21,21 +23,21 @@ func get_input():
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
 
-#func get_input():
-#	velocity.x = 0
-#	if Input.is_action_pressed("walk_right"):
-#		velocity.x += speed
-#	if Input.is_action_pressed("walk_left"):
-#		velocity.x -= speed
-
 func _physics_process(delta):
 	get_input()
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-			double_jumped = false
+			has_double_jumped = false
 			velocity.y = jump_speed
-		elif not double_jumped:
-			double_jumped = true
+		elif not has_double_jumped:
+			has_double_jumped = true
 			velocity.y = jump_speed
+	#elif Input.is_action_pressed("jump"):
+	#	velocity.y += gravity / 2 * delta
+	
+	#elif Input.is_action_just_released("jump") && velocity.y<0:
+	elif Input.is_action_just_released("jump"):	
+		velocity.y = lerp(velocity.y, hover_speed, air_friction)
+	else:
+		velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
