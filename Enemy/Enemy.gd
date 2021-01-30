6bf5917dd83e02f9492603741fa7059f588439a3
Firliminat <bobet.alexandre@gmail.com>
2021-01-30 14:12:41 +0100

    refacto Enemy to use state machine logic

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,12 +1,10 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
-export (int) var direction = -1
 export (float) var friction = 0.1
 export (float) var acceleration = 0.25
 
-const MIN_DURATION_WALK_TIMER = 1
-const MAX_DURATION_WALK_TIMER = 2
+const POSSIBLE_TIMER_DURATIONS = [0.5, 1, 1.5, 2]
 
 onready var walking_timer = $WalkingTimer
 onready var sprite = $Sprite
@@ -14,50 +12,79 @@ onready var animation_player = $AnimationPlayer
 
 var walking := true
 var velocity := Vector2.ZERO
+var direction := Vector2.LEFT
+
+enum {
+	IDLE,
+	NEW_DIRECTION,
+	WALK
+}
+
+var state = WALK
+
+func _process(delta):
+	match state:
+		IDLE:
+			idle()
+		
+		NEW_DIRECTION:
+			flip()
+			state = WALK
+			
+		WALK:
+			walk()
 
 ##########
 
 func upd_walking_timer_duration():
-	walking_timer.set_wait_time(randf() * (MAX_DURATION_WALK_TIMER - MIN_DURATION_WALK_TIMER) + MIN_DURATION_WALK_TIMER)
+	walking_timer.set_wait_time(choose(POSSIBLE_TIMER_DURATIONS))
 
 func walk():
-	if(walking):
-		velocity.x = lerp(velocity.x, speed * direction, acceleration)
-		animation_player.play("Walking")
-	else:
-		velocity.x = lerp(velocity.x, 0, friction)
-		animation_player.play("Idle")
+	velocity.x = lerp(velocity.x, speed * direction.x, acceleration)
+	animation_player.play("Walking")
+
+func idle():
+	velocity.x = lerp(velocity.x, Vector2.ZERO.x, friction)
+	animation_player.play("Idle")
 
 func flip():
 	direction = -direction
 	sprite.flip_h = !sprite.flip_h
 
-##########
+func choose(array):
+	array.shuffle()
+	return array.front()
 
-func _ready():
+func reset_timer():
 	upd_walking_timer_duration()
 	walking_timer.start()
 
+##########
+
+func _ready():
+	reset_timer()
+
 func _physics_process(delta):
-	walk()
 	velocity.y += EngineParameters.GRAVITY * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _on_WalkingTimer_timeout():
-	walking = !walking
-	upd_walking_timer_duration()
-	walking_timer.start()
-	if(walking && randf() > 0.5):
-		flip()
+	reset_timer()
+	match state:
+		IDLE:
+			state = choose([WALK, NEW_DIRECTION])
+		
+		WALK:
+			state = IDLE
 
 func _on_LeftCliffDetector_body_exited(body):
-	flip()
+	state = NEW_DIRECTION
 
 func _on_RightCliffDetector_body_exited(body):
-	flip()
+	state = NEW_DIRECTION
 
 func _on_LeftWallDetector_body_entered(body):
-	flip()
+	state = NEW_DIRECTION
 
 func _on_RightWallDetector_body_entered(body):
-	flip()
+	state = NEW_DIRECTION
