8bf520dd1651d5aafa73ddf6e29ebca1daff435f
Martin Castel <martin.castel@hotmail.fr>
2021-01-30 04:49:42 +0100

    fix double jump bug whe falling

--- Player.gd
+++ Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 onready var stand_hit_box = $StandHitBox
 onready var crawl_hit_box = $CrawlHitBox
 
-export (int) var walk_speed = 1200
+export (int) var MAX_JUMPS = 2
+
+export (int) var walk_speed = 400
 export (float, 0, 1.0) var walk_acceleration = 0.25
 
 export (int) var crawl_speed = 300
@@ -16,17 +18,16 @@ export (int) var jump_speed = -1100
 export (int) var gravity = 4000
 
 var velocity = Vector2.ZERO
-var has_double_jumped = false
+var jump_count = 0
 var is_crawling = false
 
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var air_friction = 0.5
 
+var dir
 
 func get_input():
-	var dir = 0
-	var speed
-	var acceleration
+	dir = 0
 	
 	if Input.is_action_pressed("walk_right"):
 		dir += 1
@@ -42,6 +43,29 @@ func get_input():
 		self.crawl_hit_box.disabled = true
 		is_crawling = false
 		
+	
+
+func _physics_process(delta):
+	get_input()
+	var speed
+	var acceleration
+
+	if is_on_floor():
+		jump_count = 0
+	elif jump_count == 0:
+		jump_count = 1
+
+	if Input.is_action_just_pressed("jump") && jump_count < MAX_JUMPS:
+		jump_count += 1
+		velocity.y = jump_speed
+		is_crawling = false
+
+	elif Input.is_action_just_released("jump"):	
+		velocity.y = lerp(velocity.y, hover_speed, air_friction)
+	else:
+		velocity.y += gravity * delta
+		
+	
 	if (is_crawling):
 		speed = crawl_speed
 		acceleration = crawl_acceleration
@@ -53,22 +77,5 @@ func get_input():
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
-
-func _physics_process(delta):
-	get_input()
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			has_double_jumped = false
-			velocity.y = jump_speed
-		elif not has_double_jumped:
-			has_double_jumped = true
-			velocity.y = jump_speed
-	#elif Input.is_action_pressed("jump"):
-	#	velocity.y += gravity / 2 * delta
-	
-	#elif Input.is_action_just_released("jump") && velocity.y<0:
-	elif Input.is_action_just_released("jump"):	
-		velocity.y = lerp(velocity.y, hover_speed, air_friction)
-	else:
-		velocity.y += gravity * delta
+		
 	velocity = move_and_slide(velocity, Vector2.UP)
