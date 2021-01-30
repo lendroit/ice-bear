43ae0e9ca2233900eecfa6a508aa4879456a326d
Alexandre BOBET <bobet.alexandre@gmail.com>
2021-01-30 01:49:24 +0100

    Ajout sprite pingouin & flip au demitour & demitour random à l'arrêt

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -6,14 +6,19 @@ export (int) var direction = -1
 export (float) var friction = 0.1
 export (float) var acceleration = 0.25
 
+const MIN_DURATION_WALK_TIMER = 1
+const MAX_DURATION_WALK_TIMER = 2
+
 onready var walking_timer = $WalkingTimer
+onready var sprite = $Sprite
 
 var walking := true
 var velocity := Vector2.ZERO
 
-func _ready():
-	walking_timer.set_wait_time(randf()+1)
-	walking_timer.start()
+##########
+
+func upd_walking_timer_duration():
+	walking_timer.set_wait_time(randf() * (MAX_DURATION_WALK_TIMER - MIN_DURATION_WALK_TIMER) + MIN_DURATION_WALK_TIMER)
 
 func walk():
 	if(walking):
@@ -21,21 +26,30 @@ func walk():
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
 
+func flip():
+	direction = -direction
+	sprite.flip_h = !sprite.flip_h
+
+##########
+
+func _ready():
+	upd_walking_timer_duration()
+	walking_timer.start()
+
 func _physics_process(delta):
 	walk()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-
 func _on_RightDetector_body_exited(body):
-	direction = -1
-
+	flip()
 
 func _on_LeftDetector_body_exited(body):
-	direction = 1
-
+	flip()
 
 func _on_WalkingTimer_timeout():
 	walking = !walking
-	walking_timer.set_wait_time(randf()+1)
+	upd_walking_timer_duration()
 	walking_timer.start()
+	if(walking && randf() > 0.5):
+		flip()
