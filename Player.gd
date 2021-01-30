9e8e05e403c5f6d46415f727fcab974a815be0be
Martin Castel <martin.castel@hotmail.fr>
2021-01-30 03:39:22 +0100

    add crawl feature (still need to implement collision shape change) ; and change hitbox player

--- Player.gd
+++ Player.gd
@@ -1,23 +1,47 @@
 extends KinematicBody2D
 
-export (int) var speed = 400
+export (int) var walk_speed = 1200
+export (float, 0, 1.0) var walk_acceleration = 0.25
+
+export (int) var crawl_speed = 300
+export (float, 0, 1.0) var crawl_acceleration = 0.75
+
 export (int) var hover_speed = 400
+
 export (int) var jump_speed = -1100
+
 export (int) var gravity = 4000
 
 var velocity = Vector2.ZERO
 var has_double_jumped = false
+var is_crawling = false
 
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var air_friction = 0.5
-export (float, 0, 1.0) var acceleration = 0.25
+
 
 func get_input():
 	var dir = 0
+	var speed
+	var acceleration
+	
 	if Input.is_action_pressed("walk_right"):
 		dir += 1
 	if Input.is_action_pressed("walk_left"):
 		dir -= 1
+	if Input.is_action_just_pressed("down"):
+		is_crawling = true
+		velocity.x = 0
+	elif Input.is_action_just_released("down"):
+		is_crawling = false
+		
+	if (is_crawling):
+		speed = crawl_speed
+		acceleration = crawl_acceleration
+	else:
+		speed = walk_speed
+		acceleration = walk_acceleration
+
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
