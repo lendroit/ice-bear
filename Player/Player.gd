8cddef59d05dde6331f567a974df835a72d7b6be
Firliminat <bobet.alexandre@gmail.com>
2021-01-31 16:58:28 +0100

    add water tiles that kills

--- Player/Player.gd
+++ Player/Player.gd
@@ -259,8 +259,11 @@ func _physics_process(delta):
 	else:
 		grappling_hook_rope.visible = false
 
-func _on_HurtBox_area_shape_entered(_area_id, _area, _area_shape, _self_shape):
+func player_hurt():
 	HEALTH_POINTS -= 1
+
+func _on_HurtBox_area_shape_entered(_area_id, _area, _area_shape, _self_shape):
+	player_hurt()
 
 
 func _on_PickupBox_area_entered(area):
@@ -329,3 +332,9 @@ func _play_death_sound():
 func _on_HookPositionTween_tween_all_completed():
 	hooked_node = null
 	pass # Replace with function body.
+
+
+func _on_HurtBox_body_entered(body):
+	if !(body is Water):
+		return
+	player_death()
