b9e496271c2555116f06821e2493f11399e3dc2a
Pierre Poupin <pierrep@bam.tech>
2021-01-31 23:59:30 +0100

    remove useless export variables

--- Player/PlayerParameters.gd
+++ Player/PlayerParameters.gd
@@ -1,38 +1,38 @@
-extends Node2D
+extends Node
 
 #		WALK VARIALES
-export (int) var PLAYER_WALK_SPEED = 600
-export (float, 0, 1.0) var PLAYER_WALK_ACCELERATION = 0.25
-export (float, 0, 1.0) var PLAYER_WALK_FRICTION = .55
+var PLAYER_WALK_SPEED = 600
+var PLAYER_WALK_ACCELERATION = 0.25
+var PLAYER_WALK_FRICTION = .55
 
-export (int) var PLAYER_GRAPPLING_HOOK_SPEED = 2000
+var PLAYER_GRAPPLING_HOOK_SPEED = 2000
 
 #		CRAWL VARIALES
-export (int) var PLAYER_CRAWL_SPEED = 200
-export (float, 0, 1.0) var PLAYER_CRAWL_ACCELERATION = 0.75
-export (float, 0, 1.0) var PLAYER_CRAWL_FRICTION = .9
+var PLAYER_CRAWL_SPEED = 200
+var PLAYER_CRAWL_ACCELERATION = 0.75
+var PLAYER_CRAWL_FRICTION = .9
 
 #		HOVER VARIALES
-export (int) var PLAYER_HOVER_SPEED = 225
-export (float, 0, 1.0) var PLAYER_AIR_FRICTION = .1
+var PLAYER_HOVER_SPEED = 225
+var PLAYER_AIR_FRICTION = .1
 
 #		JUMP VARIALES
-export (int) var PLAYER_JUMP_SPEED = -1300
-export (int) var PLAYER_MAX_JUMPS = 1
+var PLAYER_JUMP_SPEED = -1300
+var PLAYER_MAX_JUMPS = 1
 
 #		SHOOT VARIALES
-export (float) var PLAYER_SHOOT_TIMER_TIME = 0.5
-export var PLAYER_SPIT_VELOCITY = 1500
+var PLAYER_SHOOT_TIMER_TIME = 0.5
+var PLAYER_SPIT_VELOCITY = 1500
 
 
-export (int) var PLAYER_HEALTH_POINTS = 2
+var PLAYER_HEALTH_POINTS = 2
 
 #		POWER (DE)ACTIVATION
-export var PLAYER_CAN_GLAIRE = false
-export var PLAYER_CAN_HOVER = false
-export var PLAYER_CAN_CRAWL = false # Buggy & Unexploited feature
-export var PLAYER_CAN_HOOK = false
-export var PLAYER_CAN_BUILD = false
+var PLAYER_CAN_GLAIRE = false
+var PLAYER_CAN_HOVER = false
+var PLAYER_CAN_CRAWL = false # Buggy & Unexploited feature
+var PLAYER_CAN_HOOK = false
+var PLAYER_CAN_BUILD = false
 
 var PLAYER_SPIT_SOUNDS = [
 	preload("res://assets/sound/spit/spit1.wav"),
