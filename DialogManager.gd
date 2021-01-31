73ee849404455fe575ce4affc20747f4686f9967
Pierre Poupin <pierrep@bam.tech>
2021-01-31 16:01:55 +0100

    add different dialogs

--- DialogManager.gd
+++ DialogManager.gd
@@ -3,13 +3,20 @@ extends Control
 signal end
 
 onready var lama = $Lama
+onready var beaver = $Beaver
 
 func _ready():
 	for child in get_children():
 		child.connect("end", self, "end_dialog")
 
 func start(dialog_name: String):
-	lama.start()
+	match dialog_name:
+		"lama":
+			lama.start()
+		"beaver":
+			beaver.start()
+		_:
+			emit_signal("end")
 
 func end_dialog():
 	emit_signal("end")
--- Friends/Beaver.gd
+++ Friends/Beaver.gd
@@ -1,3 +1,6 @@
 extends "res://Friends/Friend.gd"
 
 class_name Beaver
+
+func _ready():
+	friend_name = "beaver"
