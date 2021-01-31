1a4ddc6f9527874ce135387cdb034f1406e767bf
Pierre Poupin <pierrep@bam.tech>
2021-01-31 16:36:00 +0100

    add rest of animals in dialogs

--- Friends/Crow.gd
+++ Friends/Crow.gd
@@ -1,3 +1,6 @@
 extends "res://Friends/Friend.gd"
 
 class_name Crow
+
+func _ready():
+	friend_name = "crow"
--- Friends/Snake.gd
+++ Friends/Snake.gd
@@ -1,3 +1,6 @@
 extends "res://Friends/Friend.gd"
 
 class_name Snake
+
+func _ready():
+	friend_name = "snake"
