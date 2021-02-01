4ce847d7b1493472a4c17445808c827bab909425
Alexandre BOBET <bobet.alexandre@gmail.com>
2021-02-01 23:44:28 +0100

    refactor buildable platforms

--- BuildablePlatform/BuildablePlatform.gd
+++ BuildablePlatform/BuildablePlatform.gd
@@ -1,43 +1,34 @@
 extends StaticBody2D
 
-export (float) var building_animation_time = 0.6
-
-onready var animation_player = $AnimationPlayer
-onready var build_animation_timer = $BuildAnimationTimer
 onready var body = $Body
+onready var logs = $Logs
 
 var builded := false
 var building := false
 
 func _ready():
 	unbuilded()
-
-func reset_build_animation_timer():
-	build_animation_timer.set_wait_time(building_animation_time)
-	build_animation_timer.start()
+	var _useless = $Logs.connect("platform_builded", self, "end_build")
 
 func unbuilded():
 	if(!builded && !building):
 		building = false
 		builded = false
-		animation_player.play("Unbuilded")
+		body.disabled = true
+		logs.unbuilded()
 
 func build():
 	if(!builded && !building):
 		building = true
 		builded = false
-		animation_player.play("Building")
-		reset_build_animation_timer()
+		logs.building()
 
 func end_build():
 	if(!builded && building):
 		building = false
 		builded = true
-		body.disabled = !builded
-		animation_player.play("Builded")
-
-func _on_BuildAnimationTimer_timeout():
-	end_build()
+		body.disabled = false
+		logs.builded()
 
 func _on_BeaverDetector_area_shape_entered(_area_id, _area, _area_shape, _self_shape):
 	build()
--- BuildablePlatform/Logs/Logs.gd
+++ BuildablePlatform/Logs/Logs.gd
@@ -1,26 +1,24 @@
 extends Node2D
 
+onready var building_animation = $BuildingAnimation 
+
+signal platform_builded
+
 func unbuilded():
-		$SpriteContainer/Unbuilded.visible = true
-		$SpriteContainer/Building.visible = false
-		$SpriteContainer/Builded.visible = false
+		$Unbuilded.visible = true
+		$Building.visible = false
+		$Builded.visible = false
 
 func building():
-		$SpriteContainer/Unbuilded.visible = false
-		$SpriteContainer/Building.visible = true
-		$SpriteContainer/Builded.visible = false
-		animation_player.play("Building")
+		$Unbuilded.visible = false
+		$Building.visible = true
+		$Builded.visible = false
+		building_animation.play("Building")
 
 func builded():
-		$SpriteContainer/Unbuilded.visible = false
-		$SpriteContainer/Building.visible = false
-		$SpriteContainer/Builded.visible = true
+		$Unbuilded.visible = false
+		$Building.visible = false
+		$Builded.visible = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_BuildingAnimation_animation_finished(_anim_name):
+	emit_signal("platform_builded")
