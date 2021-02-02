d48f36b57e355c0402f641ac26feb2be5f13cf23
Alexandre BOBET <bobet.alexandre@gmail.com>
2021-02-02 01:13:55 +0100

    rename enemy glaire grad coeff

--- Enemy/EnemyGlaire.gd
+++ Enemy/EnemyGlaire.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
 var velocity := Vector2.ZERO
-const SPIT_DRAG_COEFF := 2.0
+const ENEMY_GLAIRE_DRAG_COEFF := 2.0
 
 func _process(delta):
-	velocity.y += EngineParameters.GRAVITY / SPIT_DRAG_COEFF * delta
+	velocity.y += EngineParameters.GRAVITY / ENEMY_GLAIRE_DRAG_COEFF * delta
 	position += velocity * delta
 	rotation = velocity.angle()
 
