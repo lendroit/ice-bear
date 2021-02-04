90c401984363c2c03fb4a95cf4f4073c3e1cc95d
Pierre Poupin <pierrep@bam.tech>
2021-02-04 01:23:02 +0100

    remove dead Water code

--- Terrain/Water.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-signal PlayerInDeathZone
-
-func _on_DeathZone_body_shape_entered(body_id, body, body_shape, area_shape):
-	print("death zone for ", body_id)
-	emit_signal("PlayerInDeathZone")
