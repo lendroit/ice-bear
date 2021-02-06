402bd86917f499532348c4ab81e25e5fff0fcdc0
Pierre Poupin <pierrep@bam.tech>
2021-02-06 01:31:54 +0100

    add shitting bird animation

--- Enemy/Bird/Bird.gd
+++ Enemy/Bird/Bird.gd
@@ -22,6 +22,7 @@ func _ready():
 	reset_shoot_timer()
 
 func shoot():
+	$ShootAnimationPlayer.play("StartShitting")
 	var new_shit = Shit.instance()
 	new_shit.position = self.position
 	emit_signal("bird_shot", new_shit)
