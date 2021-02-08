2d83c6394b03dc360e1b771af82a028248028358
Pierre Poupin <pierrep@bam.tech>
2021-02-08 19:53:30 +0100

    improve shit animation

--- Enemy/EnemyShit.gd
+++ Enemy/EnemyShit.gd
@@ -7,6 +7,9 @@ signal shit_exploded
 const SPEED = 30
 const ENEMY_GLAIRE_DRAG_COEFF := 2.0
 
+onready var animation_player = $AnimationPlayer
+onready var collision_shape = $CollisionShape2D
+
 func _ready():
 	contact_monitor = true
 	contacts_reported = 1
@@ -14,6 +17,15 @@ func _ready():
 func _integrate_forces(state):
 	add_force(Vector2.ZERO, SPEED * Vector2.DOWN)
 
+func _disable_shit():
+	set_deferred("mode", RigidBody2D.MODE_STATIC)
+	collision_shape.set_deferred("disabled", true)
+
 func _on_EnemyGlaire_body_entered(body):
 	emit_signal("shit_exploded", self)
-	queue_free()
+	_disable_shit()
+	animation_player.play("Death")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if (anim_name == "Death"):
+		queue_free()
