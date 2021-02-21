57daa04ffffbccb3af6a22b12069aca9757a452c
Pierre Poupin <pierrep@bam.tech>
2021-02-21 03:02:54 +0100

    feature(flag): add flag animation

--- Terrain/Flag/Flag.gd
+++ Terrain/Flag/Flag.gd
@@ -1,6 +1,13 @@
 tool
 extends Node2D
 
+onready var animation_player = $AnimationPlayer
+
 func set_is_active(new_is_active):
+	if new_is_active:
+		animation_player.play("Activate")
+	else:
+		animation_player.play("Inactive")
+
 	$Sprites/flag.visible = new_is_active
 	$Sprites/raplapla.visible = !new_is_active
