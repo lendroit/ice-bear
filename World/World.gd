2182c880b7e395ef35cb1b369fe244a2efbf9208
Pierre Poupin <pierrep@bam.tech>
2021-02-21 02:00:16 +0100

    refactor(World): clean-up script

--- World/World.gd
+++ World/World.gd
@@ -7,19 +7,19 @@ var theme_sound = preload("res://assets/sound/theme.wav")
 onready var dialog_handler = $CanvasLayer/DialogManager
 onready var theme = $AudioPlayer/theme
 onready var global_player_spawn = $PlayerSpawn
+onready var player = $Player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	_play_theme()
-	var _useless = $Player.connect("player_died", self, "_player_died")
-	var _useless2 = $Player.connect("player_win", self, "_player_win")
+	var _useless = player.connect("player_died", self, "_player_died")
+	var _useless2 = player.connect("player_win", self, "_player_win")
 	dialog_handler.connect("end", self, "end_dialog")
 	var enemies = get_tree().get_nodes_in_group("enemy")
 	for enemy in enemies:
 		enemy.connect("died", self, "add_explosion")
 		if enemy is Bird:
 			enemy.connect("bird_shot", self, "add_bird_shot")
-	
+
 	var friends = get_tree().get_nodes_in_group("friend")
 	for friend in friends:
 		friend.connect("trigger_dialog", self, "open_dialog")
@@ -29,10 +29,9 @@ func _ready():
 		checkpoint.connect("checkpoint_activated", self, "_activate_checkpoint")
 
 func _player_died():
-	$Player.velocity = Vector2.ZERO
-	$Player.global_position = global_player_spawn.global_position
-	# var _useless = get_tree().reload_current_scene()
-	
+	player.velocity = Vector2.ZERO
+	player.global_position = global_player_spawn.global_position
+
 func _player_win():
 	var _useless = get_tree().change_scene("res://End/End.tscn")
 
@@ -41,12 +40,11 @@ func add_explosion(position: Vector2):
 	d.position = position
 	self.add_child(d)
 	d.emitting = true
-	pass
 
 func open_dialog(friend_name: int):
 	get_tree().paused = true
 	dialog_handler.start(friend_name)
-	
+
 func end_dialog():
 	get_tree().paused = false
 
